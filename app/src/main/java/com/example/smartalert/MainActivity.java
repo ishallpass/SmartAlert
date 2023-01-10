539f3ae7544450c12b1012a7c770a68882ed33e2
ishallpass <kostikasjannakis123@gmail.com>
2023-01-10 15:34:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/smartalert/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartalert;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore firestore;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
